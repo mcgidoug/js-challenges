6c2ac02c9445e561da45da8ee428deae60c84c32
mcgidoug <mcgidoug@gmail.com>
2022-10-18 11:45:05 -0500

    completed array plus array challenge

---
+++ previous-solutions/arrayPlusArray.js
@@ -0,0 +1,20 @@
+arr1 = [1, 2];
+arr2 = [3, 4];
+
+function arrayPlusArray(arr1, arr2) {
+  let sum = 0;
+
+  for (let i = 0; i < 2; i++) {
+    const arr1Value = arr1[i];
+    const arr2Value = arr2[i];
+
+    const addedValue = arr1Value + arr2Value;
+
+    sum = sum + addedValue;
+  }
+  return sum;
+}
+
+console.log(arrayPlusArray(arr1, arr2).toString());
+
+// expected: 10
