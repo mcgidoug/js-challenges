70427aad66a08177f8812e73756ab435ade48e8e
mcgidoug <mcgidoug@gmail.com>
2022-11-12 13:14:10 -0600

    completed 7kyu descending order solution

---
+++ codewars/7kyu/descendingOrder.js
@@ -0,0 +1,15 @@
+let n = 0;
+
+function descendingOrder(n) {
+  if (n.length === 1) {
+    return n;
+  }
+  const arrOfDigits = Array.from(String(n), Number);
+  const descOrder = parseInt(arrOfDigits.sort().reverse().join(""));
+
+  return descOrder;
+}
+
+console.log(descendingOrder(n));
+
+// expected: [1, 2, 3, 4]
