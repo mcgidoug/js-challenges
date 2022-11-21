1f9b78887a2dea7b0a40fd351f03b3b9d5bcf8ef
mcgidoug <mcgidoug@gmail.com>
2022-11-21 14:29:47 -0600

    completed 7kyu sum of two lowest positive integers solution

---
+++ codewars/7kyu/sumOfTwoLowPosInt.js
@@ -0,0 +1,15 @@
+let numbers = [5, 8, 12, 19, 22];
+
+function sumTwoSmallestNumbers(numbers) {
+  let sortedNumbers = numbers.sort(function (a, b) {
+    return a - b;
+  });
+
+  for (let i = 0; i < sortedNumbers.length; i++) {
+    return sortedNumbers[0] + sortedNumbers[1];
+  }
+
+  return sortedNumbers;
+}
+
+console.log(sumTwoSmallestNumbers(numbers));
