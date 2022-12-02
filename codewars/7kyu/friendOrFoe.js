02f5f9ef1b6c6c793050ae228d15f4832e8168ff
mcgidoug <mcgidoug@gmail.com>
2022-12-02 10:30:11 -0600

    updated 7kyu friend or foe solution

--- codewars/7kyu/friendOrFoe.js
+++ codewars/7kyu/friendOrFoe.js
@@ -1,3 +1,19 @@
 function friend(friends) {
   return friends.filter((word) => word.length == 4);
 }
+
+// or...
+
+let friends = ["Doug", "Kieran", "Mark", "Ryan"];
+
+function friend(friends) {
+  let realFriends = [];
+  for (let i = 0; i < friends.length; i++) {
+    if (friends[i].length == 4) {
+      realFriends.push(friends[i]);
+    }
+  }
+  return realFriends;
+}
+
+console.log(friend(friends));
