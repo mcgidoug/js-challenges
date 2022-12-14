9605904be83210ea08b3583cbeb4525a1eddd872
mcgidoug <mcgidoug@gmail.com>
2022-12-14 16:46:28 -0600

    completed 8kyu logic drill traffic light solution

---
+++ codewars/8kyu/logicDrillTrafficLight.js
@@ -0,0 +1,15 @@
+let current = "green";
+
+function updateLight(current) {
+  if (current === "green") {
+    return "yellow";
+  }
+  if (current === "yellow") {
+    return "red";
+  }
+  if (current === "red") {
+    return "green";
+  }
+}
+
+console.log(updateLight(current));
