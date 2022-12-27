a0f3468f94a8b5c0049d4784e0c257ab634b24b7
Doug <mcgidoug@gmail.com>
2022-12-27 14:58:29 -0600

    completed 8kyu count by x solution

---
+++ codewars/8kyu/countByX.js
@@ -0,0 +1,14 @@
+// count by x
+
+function countBy(x, n) {
+    let z = [];
+  
+    for (var i = 1; i <= n; i++) {
+      z.push(x * i);
+    }
+  
+  return z;
+  }
+  
+  console.log(countBy(3, 5));
+  
--- main/app.js
+++
@@ -1,9 +0,0 @@
-// count by x
-
-function countBy(x, n) {
-  let z = [];
-
-  return z;
-}
-
-console.log(countBy(x, n));
