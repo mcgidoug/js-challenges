153e1185df60aea9b7df749e1ad9ea70f1724778
mcgidoug <mcgidoug@gmail.com>
2023-01-02 11:00:09 -0600

    updated 6kyu duplicate encoder solution with some pseudo code

--- codewars/6kyu/duplicateEncoder.js
+++ codewars/6kyu/duplicateEncoder.js
@@ -21,3 +21,21 @@ console.log(duplicateEncode("din")); // "((("
 console.log(duplicateEncode("Hello World")); // "(()))(()()("
 console.log(duplicateEncode("ooldd world")); // ")))))(()())"
 console.log(duplicateEncode("Success")); // ")())())"
+
+// note: here is what the object looks like before sorting and returning parenthesis
+
+function duplicateEncode(word) {
+  const charCount = {};
+  word
+    .toLowerCase()
+    .split("")
+    .forEach((char) => {
+      charCount[char] = (charCount[char] || 0) + 1;
+    });
+
+  return charCount;
+}
+
+console.log(duplicateEncode("hello"));
+
+// {h: 1, e: 1, l: 2, o: 1}
