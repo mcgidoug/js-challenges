3130237681453e053d1144605beee51e8f4f84bc
Doug <mcgidoug@gmail.com>
2023-01-04 12:36:20 -0600

    modified 6kyu array diff solution with note

--- codewars/6kyu/arrayDiff.js
+++ codewars/6kyu/arrayDiff.js
@@ -2,6 +2,7 @@ let a = [1,2,3,4]
 let b = [2,4]
 
 function arrayDiff(a, b) {
+  // creates a new array that contains all elements of a that are NOT in b
   let filter = a.filter(item => !b.includes(item))
   return filter;
 }
