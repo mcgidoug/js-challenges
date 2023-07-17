77565c0ffc10065e44b76d8472efada0d96636f4
mcgidoug <mcgidoug@gmail.com>
2023-07-17 16:43:20 -0500

    added welcome challenge v2

---
+++ codewars/8kyu/welcomeV2.js
@@ -0,0 +1,29 @@
+function greet(language) {
+  let languages = {
+    english: "Welcome",
+    czech: "Vitejte",
+    danish: "Velkomst",
+    dutch: "Welkom",
+    estonian: "Tere tulemast",
+    finnish: "Tervetuloa",
+    flemish: "Welgekomen",
+    french: "Bienvenue",
+    german: "Willkommen",
+    irish: "Failte",
+    italian: "Benvenuto",
+    latvian: "Gaidits",
+    lithuanian: "Laukiamas",
+    polish: "Witamy",
+    spanish: "Bienvenido",
+    swedish: "Valkommen",
+    welsh: "Croeso",
+  };
+
+  if (language && languages[language]) {
+    return `${languages[language]}`;
+  } else {
+    return `Welcome`;
+  }
+}
+
+console.log(greet("hi"));
